b9a3fc9ca79f0b0bd86ab3428a25be14a89dcc85
App Academy Student <student@appacademy.io>
2015-11-06 16:08:05 -0800

    finished p03

--- my_hash_map/lib/p02_hashing.rb
+++ my_hash_map/lib/p02_hashing.rb
@@ -4,15 +4,55 @@ end
 
 class Array
   def hash
+    return 17 if self.empty?
+
+    hashes = self.map do |el|
+        unless el.class == Fixnum
+          el = el.hash
+        else
+          el
+        end
+      end
+
+    size = self.length
+
+    num_s = "17"
+    hashes.each do |i|
+      num_s += i.to_s
+    end
+
+    secret_number = num_s.to_i
+
+    secret_number2 = hashes.inject(69) do |acc, num|
+      acc = size * hashes.first + (23 * num * 2) + 3 * hashes.last + acc
+    end
+
+    secret_number2 = secret_number2.to_s[0...10].to_i
+
+    num = (secret_number2.abs + (secret_number * secret_number2.abs) + size)
+    num.to_s[0...10].to_i
   end
+
+
+
 end
 
 class String
   def hash
+    array = self.chars.map(&:ord)
+    array.hash
   end
 end
 
 class Hash
   def hash
+    array = []
+
+    self.each do |k, v|
+      array << k.to_s.hash
+      array << v.to_s.hash
+    end
+
+    array.sort.hash
   end
 end
--- my_hash_map/lib/p03_hash_set.rb
+++ my_hash_map/lib/p03_hash_set.rb
@@ -9,18 +9,30 @@ class HashSet
   end
 
   def insert(key)
+    if include?(key)
+      false
+    else
+      @count += 1
+      resize! if @count == num_buckets
+      @store[key.hash % num_buckets] << key.hash
+      true
+    end
   end
 
   def include?(key)
+    @store[(key.hash % num_buckets).abs].include?(key.hash)
   end
 
   def remove(key)
+    @store[key.hash % num_buckets].delete(key.hash)
+    @count -= 1
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num]
   end
 
   def num_buckets
@@ -28,5 +40,12 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |buck|
+      buck.each do |set_element|
+        new_store[set_element % new_store.length] << set_element
+      end
+    end
+    @store = new_store
   end
 end
